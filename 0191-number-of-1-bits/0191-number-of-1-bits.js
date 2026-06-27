65aa07b953586ebf12b8475abc83800990f21a53
Mufeedaa-kp <mufeedhamufi555@gmail.com>
2026-06-27 11:46:48 +0530

    Time: 3 ms (3.76%), Space: 55.2 MB (26.81%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let binary = n.toString(2)
+    let count  = 0
+
+    for(let bit of binary){
+        if(bit === "1"){
+            count++
+        }
+    }
+    return count
+};
