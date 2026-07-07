999a357882b410643348942151c09be6915aafdc
Mufeedaa-kp <mufeedhamufi555@gmail.com>
2026-07-07 11:22:09 +0530

    Time: 47 ms (38.78%), Space: 52.7 MB (89.8%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function(command) {
+    let result = "";
+
+    for (let i = 0; i < command.length; i++) {
+
+        if (command[i] === "G") {
+            result += "G";
+        }
+        else if (command[i] === "(" && command[i + 1] === ")") {
+            result += "o";
+            i++;
+        }
+        else {
+            result += "al";
+            i += 3;
+        }
+    }
+
+    return result;
+};
